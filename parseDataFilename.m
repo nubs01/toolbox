e9a708d6d0b5bfe7bdb267231c76312b63ff60b5
Roshan Nanu <rnanu@brandeis.edu>
2018-06-25 17:08:53 -0400

    Function to parse data filenames

---
+++ parseDataFilename.m
@@ -0,0 +1,20 @@
+function out = parseDataFilename(fn,animID)
+    % Given a data filename fn (s.a. 'RW6eeg01-03-02.mat') and an animal ID
+    % ('RW6') this will return a structure containing the data type (eeg) and
+    % day, epoch and tetrode in a struct with fields type, day, epoch, tetrode
+
+    pat = [animID '(?<type>\D*)(?<day>\d+)-(?<epoch>\d+)-(?<tet>\d+).mat'];
+    parsed = regexp(fn,pat,'names');
+    if ~isempty(parsed)
+        out = parsed;
+        return;
+    end
+    pat = [animID '(?<type>\D*)(?<day>\d+)-(?<epoch>\d+).mat'];
+    parsed = regexp(fn,pat,'names');
+    if ~isempty(parsed)
+        out = parsed;
+        return;
+    end
+    pat = [animID '(?<type>\D*)(?<day>\d+).mat'];
+    out = regexp(fn,pat,'names');
+    
