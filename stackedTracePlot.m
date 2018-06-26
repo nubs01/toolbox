8ad4609d9893de419bc9247f6b6e0658a0f10a94
Roshan Nanu <rnanu@brandeis.edu>
2018-06-26 11:55:51 -0400

    New Function to plot all tetrode traces from an epoch and highlight chosen parts

---
+++ stackedTracePlot.m
@@ -0,0 +1,22 @@
+function out = stackedTracePlot(X,Y,highlightIdx)
+    % Plots the rows of Y in their own row of the plot and highlight the
+    % regions denoted by highlightIdx in red
+
+    % determine Y range of each row
+    rng = range(Y,2);
+    maxRng = max(rng);
+
+    % remove means from each row
+    Y2 = Y-mean(Y,2);
+
+    % Plot each row in black and higlights in red
+    out = figure();
+    hold on
+    for l=1:size(Y,1)
+        plot(X,Y2(l,:)+(l-1)*maxRng,'k','LineWidth',2)
+        for k=1:size(highlightIdx,1)
+            a = highlightIdx(k,1);
+            b = highlightIdx(k,2);
+            plot(X(a:b),Y2(l,a:b),'r','LineWidth',2)
+        end
+    end
